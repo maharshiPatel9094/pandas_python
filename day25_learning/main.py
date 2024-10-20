24ef66c3c91b805ea4fbdd18b4aac16554edc0aa
Maharshi Patel <144848033+maharshiPatel9094@users.noreply.github.com>
2024-10-20 12:50:09 -0300

    panda setup

---
+++ day25_learning/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+ 
+data = pd.read_csv("./weather_data.csv")
+print(data["temp"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import csv
+# with open("./weather_data.csv") as data:
+#     data = csv.reader(data)
+#     print(data)
+#     for row in data:
+#         print(row)
+
+
+
+
+
+# with open("./weather_data.csv") as data:
+#     data = data.readlines()
+#     print(data)
+
