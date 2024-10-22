10a0708ce79ec21c210cb1cd88c8df65cb900b35
Maharshi Patel <144848033+maharshiPatel9094@users.noreply.github.com>
2024-10-22 10:42:42 -0300

    commit
    learning dataframes and reading csv data using pandas

--- day25_learning/main.py
+++ day25_learning/main.py
@@ -1,36 +1,59 @@
-import pandas as pd
- 
-data = pd.read_csv("./weather_data.csv")
-print(data["temp"])
+import pandas as pd 
 
-# data_dict = data.to_dict()
-# print(data_dict)
-
-
-
+# Reading the CSV file with an explicit header
+try:
+    data = pd.read_csv(r"C:\Users\mahar\OneDrive\Documents\GitHub\pandas_python\day25_learning\weather_data.csv", header=0)  # Ensure first row is header
+    # print("DataFrame contents:\n", data)  # Print the entire DataFrame
+except FileNotFoundError:
+    print("File not found. Check the path.")
 
 
 
+# creating a dataframe from scratch
+data_dict = {
+    "students" : ["Amy","james","angela"],
+    "scores" : [76,56,65]
+}
 
+data_student = pd.DataFrame(data_dict)
+# print(data_student)
+data_student.to_csv(r"C:\Users\mahar\OneDrive\Documents\GitHub\pandas_python\day25_learning\new_data.csv")
 
-
-
-
-
-
-
-# import csv
-# with open("./weather_data.csv") as data:
-#     data = csv.reader(data)
-#     print(data)
-#     for row in data:
-#         print(row)
 
 
 
 
 
-# with open("./weather_data.csv") as data:
-#     data = data.readlines()
-#     print(data)
+# data_dict = data.to_dict()
+# # print(data_dict)
 
+
+# # getting data in row
+# days = data[data.day == "Monday"]
+# # print(days)
+
+# # get the row with maximum temperature
+# max_row_temp = data[data.temp == data.temp.max()]
+# # print(max_row_temp )
+# # we can also get access to this row
+# # print(max_row_temp.temp * 9/5 + 32)
+
+
+
+# # series is a coloumn 
+# # data_temp = data["temp"]
+# # data_temp = data.temp
+# data_temp_list = data.temp.to_list()
+# # print(data_temp_list)
+
+# # -----max
+# max_temp = data.temp.max()
+# # print(max_temp)
+# # ---------------avg
+# # print(data.temp.mean())
+
+# # -----------avg
+# # total = 0
+# # for temp in data_temp_list:
+# #     total += temp
+# # print(f"average  temperature is: {total/len(data_temp_list)}")
