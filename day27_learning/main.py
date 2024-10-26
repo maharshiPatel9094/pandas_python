60c8ece3aa196d16cf6d49170fb6812b7ed8bb11
Maharshi Patel <144848033+maharshiPatel9094@users.noreply.github.com>
2024-10-26 12:05:15 -0300

    commit

--- day27_learning/main.py
+++ day27_learning/main.py
@@ -1,5 +1,13 @@
 import tkinter
 
+# event listners
+def button_click():
+    print("I got clicked")
+
+
+def new_button_clicked():
+    print("I am a new button")
+
 # creating a new window and configurations
 window = tkinter.Tk()
 window.title("Widget Window")
@@ -8,15 +16,20 @@ window.minsize(width=500,height=500)
 # labels
 my_label = tkinter.Label(text="I am a old label")
 my_label.config(text="I am a new label")
-my_label["text"] = "I am a new label"
-my_label.pack()
+# my_label["text"] = "I am a new label"
+# my_label.pack()
+my_label.grid(column=1,row=1)
 
 # buttons
 # handel some events by button click
-def button_click():
-    print("I got clicked")
 my_button = tkinter.Button(text="click me",command=button_click)
-my_button.pack()
+# my_button.pack()
+my_button.grid(column=2,row=2)
+
+
+new_button = tkinter.Button(text="new_button",command=new_button_clicked)
+new_button.grid(column=3,row=1)
+
 
 
 # entries
@@ -25,104 +38,105 @@ entry = tkinter.Entry(width=30)
 entry.insert(tkinter.END,string="some text to begin with")
 # return the input string
 print(entry.get())
-entry.pack()
+# entry.pack()
+entry.grid(column=4,row=3)
 
-# text box
-text = tkinter.Text(height=5,width=10)
-# puts the cursor in textbox
-text.focus()
-# add some text to begin with 
-text.insert(tkinter.END,"Example of multi line text entry")
-text.pack()
+# # text box
+# text = tkinter.Text(height=5,width=10)
+# # puts the cursor in textbox
+# text.focus()
+# # add some text to begin with 
+# text.insert(tkinter.END,"Example of multi line text entry")
+# text.pack()
 
-# spinbox
-def spinbox_used():
-    # gets the current value in spinbox
-    print(spinbox.get())
+# # spinbox
+# def spinbox_used():
+#     # gets the current value in spinbox
+#     print(spinbox.get())
 
-spinbox = tkinter.Spinbox(from_=0,to=10,width=5,command=spinbox_used)
-spinbox.pack()
+# spinbox = tkinter.Spinbox(from_=0,to=10,width=5,command=spinbox_used)
+# spinbox.pack()
 
-# scale
-def scale_used(value):
-    print(value)
+# # scale
+# def scale_used(value):
+#     print(value)
 
-scale = tkinter.Scale(from_=0,to=100,command=scale_used)
-scale.pack()
+# scale = tkinter.Scale(from_=0,to=100,command=scale_used)
+# scale.pack()
 
 
-# checkbox
-def checkbutton_used():
-    # prints 1 if on button otherwise 0
-    print(checked_state.get())
+# # checkbox
+# def checkbutton_used():
+#     # prints 1 if on button otherwise 0
+#     print(checked_state.get())
     
-# variable to hold the state
-checked_state = tkinter.IntVar()
-checkbutton = tkinter.Checkbutton(text="is on ?",variable=checked_state,command=checkbutton_used)
-checked_state.get()
-checkbutton.pack()
+# # variable to hold the state
+# checked_state = tkinter.IntVar()
+# checkbutton = tkinter.Checkbutton(text="is on ?",variable=checked_state,command=checkbutton_used)
+# checked_state.get()
+# checkbutton.pack()
 
 
-# radiobuttons
-def radio_used():
-    print(radio_state.get())
+# # radiobuttons
+# def radio_used():
+#     print(radio_state.get())
     
-radio_state = tkinter.IntVar()
-radiobutton1 = tkinter.Radiobutton(text="option1",value=1,variable=radio_state,command=radio_used)
-radiobutton2 = tkinter.Radiobutton(text="option2",value=2,variable=radio_state,command=radio_used)
-radiobutton1.pack()
-radiobutton2.pack()
+# radio_state = tkinter.IntVar()
+# radiobutton1 = tkinter.Radiobutton(text="option1",value=1,variable=radio_state,command=radio_used)
+# radiobutton2 = tkinter.Radiobutton(text="option2",value=2,variable=radio_state,command=radio_used)
+# radiobutton1.pack()
+# radiobutton2.pack()
 
 
-# listbox
-def listbox_used(event):
-    print(listbox.get(listbox.curselection()))
+# # listbox
+# def listbox_used(event):
+#     print(listbox.get(listbox.curselection()))
     
-listbox = tkinter.Listbox(height=4)
-fruits = ["apple","pear","orange","banana"]
-for item in fruits:
-    listbox.insert(fruits.index(item),item)
+# listbox = tkinter.Listbox(height=4)
+# fruits = ["apple","pear","orange","banana"]
+# for item in fruits:
+#     listbox.insert(fruits.index(item),item)
     
-listbox.bind("<<ListboxSelect>>",listbox_used)
-listbox.pack()
-
-
-# keep the window open
-window.mainloop()
-
-
-# # tkinter
-# import tkinter
-# # object
-# window = tkinter.Tk()
-# window.title("My First GUI Program")
-# window.minsize(width= 500,height=300)
-
-# # label
-# my_label = tkinter.Label(text="I am a label",font=("Arial",24,"bold")) 
-# my_label.pack()
-
-# # ways to change the arg or update the properties of the component 
-# # my_label["text"] = "New Text"
-# # my_label.config(text="new text")
-
-# # button
-# def button_clicked():
-#     # get() -> return the input string
-#     new_text = input.get()
-#     my_label.config(text=new_text)
-
-
-
-# my_button = tkinter.Button(text="Click me",command=button_clicked)
-# my_button.pack()
-
-
-# # entry
-
-# input = tkinter.Entry(width=10)
-# input.pack()
+# listbox.bind("<<ListboxSelect>>",listbox_used)
+# listbox.pack()
 
 
 # # keep the window open
 # window.mainloop()
+
+
+# # # tkinter
+# # import tkinter
+# # # object
+# # window = tkinter.Tk()
+# # window.title("My First GUI Program")
+# # window.minsize(width= 500,height=300)
+
+# # # label
+# # my_label = tkinter.Label(text="I am a label",font=("Arial",24,"bold")) 
+# # my_label.pack()
+
+# # # ways to change the arg or update the properties of the component 
+# # # my_label["text"] = "New Text"
+# # # my_label.config(text="new text")
+
+# # # button
+# # def button_clicked():
+# #     # get() -> return the input string
+# #     new_text = input.get()
+# #     my_label.config(text=new_text)
+
+
+
+# # my_button = tkinter.Button(text="Click me",command=button_clicked)
+# # my_button.pack()
+
+
+# # # entry
+
+# # input = tkinter.Entry(width=10)
+# # input.pack()
+
+
+# # # keep the window open
+# # window.mainloop()
--- day27_learning/playgroud.py
+++ day27_learning/playgroud.py
@@ -1,14 +1,59 @@
-class Car:
-    def __init__ (self,**kwargs):
-        self.make = kwargs["make"]
-        self.model = kwargs["model"]
+import tkinter
+
+# event listners
+def button_click():
+    print("I got clicked")
 
 
-my_car = Car(make="Nissan",model="gtr")
-# print(my_car.model)
-# print(my_car.make)
-print(my_car["model"])
-print(my_car["make"])
+def new_button_clicked():
+    print("I am a new button")
+
+# creating a new window and configurations
+window = tkinter.Tk()
+window.title("Widget Window")
+window.minsize(width=500,height=500)
+
+# labels
+my_label = tkinter.Label(text="I am a old label")
+my_label.config(text="I am a new label")
+# my_label["text"] = "I am a new label"
+# my_label.pack()
+my_label.grid(column=1,row=1)
+
+# buttons
+# handel some events by button click
+my_button = tkinter.Button(text="click me",command=button_click)
+# my_button.pack()
+my_button.grid(column=2,row=2)
+
+
+new_button = tkinter.Button(text="new_button",command=new_button_clicked)
+new_button.grid(column=3,row=1)
+
+
+
+# entries
+entry = tkinter.Entry(width=30)
+# entry box will display with inb uilt text written for u
+entry.insert(tkinter.END,string="some text to begin with")
+# return the input string
+print(entry.get())
+# entry.pack()
+entry.grid(column=4,row=3)
+
+
+
+# class Car:
+#     def __init__ (self,**kwargs):
+#         self.make = kwargs["make"]
+#         self.model = kwargs["model"]
+
+
+# my_car = Car(make="Nissan",model="gtr")
+# # print(my_car.model)
+# # print(my_car.make)
+# print(my_car["model"])
+# print(my_car["make"])
 
 
 # def calculate(n,**kwargs):
